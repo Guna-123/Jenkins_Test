2abe2102924fa6f2ccb53ee82600f5ecdeca97a2
Guna Sekhar <gunasekharbellamkonda@gmail.com>
2023-10-11 21:22:32 +0530

    jenkins Commit

---
+++ JenkinsProject/src/test/java/Jenkins/JenkinsProject/Testclass.java
@@ -0,0 +1,29 @@
+package Jenkins.JenkinsProject;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Testclass {
+	
+	@Test
+	public void name() throws InterruptedException {
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.get("https://www.facebook.com/login/");
+		Thread.sleep(2000);
+		org.testng.Assert.assertTrue(driver.getTitle().contains("Facebook"), "Title is not Matched");
+//		(driver.getTitle().contains("FaceBook"), false);
+		driver.quit();
+	}
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		
+	}
+
+}
